be2ed515103254b231feb6ff026f96589a12aeca
RitterVomNi <heinen.m@web.de>
2016-12-24 12:27:01 +0100

    added project creation

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,10 @@
 class DashboardController < ApplicationController
 
   def index
+    @projects = Project.all
+  end
 
+  def new_project
+    Project.create(name: params[:title])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161224000712) do
+ActiveRecord::Schema.define(version: 20161224091936) do
 
   create_table "dashboards", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
